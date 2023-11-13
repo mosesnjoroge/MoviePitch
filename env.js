46a4eaa90982195214c6c2f79bba6816fe02a2ed
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-11-12 22:36:18 -0500

    restore env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
-// export const process = {
-//     env: {
-//
-//     }
-// }
+export const process = {
+    env: {
+        OPENAI_API_KEY: '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'
+    }
+}
